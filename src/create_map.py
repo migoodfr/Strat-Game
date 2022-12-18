0e981faa3fa7cadaf1033f66b0ac46f9378e25c6
migoodfr <113857345+migoodfr@users.noreply.github.com>
2022-12-18 18:15:37 +0100

    Add files via upload

--- src/create_map.py
+++ src/create_map.py
@@ -1,70 +1,30 @@
 from random import choice,randint
+import pygame
 
 from PIL import Image
 
-dimensions = (100, 100)
-map = Image.new("RGBA", dimensions)
 
-# 0 = ground
-# 1 = océan
+class Map:
+    """
 
-def get_randomdata_list(ground_density):
-    """génère une liste de dimensions fournies avec des valeurs semi-aléatoires 
-    (ground_density représente le pourcentage de 0 (ground) dans la liste) """
-    list_data_tile = []
-    for x in range(0, dimensions[0]):
-        list_data_tile.append([])
-        for y in range(0, dimensions[1]):
-            nb_random = randint(1,100)
-            if nb_random < ground_density:
-                list_data_tile[x].append(0)
-            else:
-                list_data_tile[x].append(1)
-    return list_data_tile
+    """
 
-def automata_cellular_algorithm(liste_data,list_dimensions):
-    """Pour chaque pixel, regarde tous les pixels alentours (carré de 3x3 pixels dont le centre est le pixel) :
-     -  si il y a qautre 0 (ground) : change le pixel en 0 (ground)
-     -  sinon change le pixel en océan (1)"""
-    for x in range(1, list_dimensions[0]-1):
-        for y in range(1, list_dimensions[1]-1):
-            nb_ground = 0
-            if liste_data[x][y] == 0:
-                nb_ground += 1
-            if liste_data[x][y+1] == 0:
-                nb_ground += 1
-            if liste_data[x][y-1] == 0:
-                nb_ground += 1
-            if liste_data[x-1][y+1] == 0:
-                nb_ground += 1
-            if liste_data[x-1][y] == 0:
-                nb_ground += 1
-            if liste_data[x-1][y-1] == 0:
-                nb_ground += 1
-            if liste_data[x+1][y+1] == 0:
-                nb_ground += 1
-            if liste_data[x+1][y] == 0:
-                nb_ground += 1
-            if liste_data[x+1][y-1] == 0:
-                nb_ground += 1
-            if nb_ground > 4 :
-                liste_data[x][y] = 0
-            else:
-                liste_data[x][y] = 1
-    return liste_data
+    def __init__(self, size_tile: int, rows: int, cols: int, color: tuple):
+        self.size_tile = size_tile
+        self.rows = rows
+        self.cols = cols
+        self.color = color
 
-def convert_list_to_image(list, list_dimensions):
-    """transforme la liste en image"""
-    for x in range(0, list_dimensions[0]):
-        for y in range(0, list_dimensions[1]):
-            if list[x][y] == 1:
-                map.putpixel((x, y), (0, 0, 255))
-            if list[x][y] == 0:
-                map.putpixel((x, y), (0, 255, 0))
+    def draw(self, screen):
+        """
+        Dessine le cadrillage il faudra remplacer par les photos des deux differents carreaux
+        """
 
-def create_map():
-    list_tile = get_randomdata_list(45)
-    list_tile = automata_cellular_algorithm(list_tile,dimensions)
-    convert_list_to_image(list_tile,dimensions)
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if (col + row) % 2 == 1:
+                    pygame.draw.rect(screen, self.color, pygame.Rect(col * self.size_tile, row * self.size_tile, self.size_tile, self.size_tile))
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(col * self.size_tile, row * self.size_tile, self.size_tile, self.size_tile))
 
-    map.show()
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,69 @@
-import create_map
+import pygame
+import random
+
+from create_map import Map
+from cameragroup import CameraGroup
+
+# Constantes
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+COLOR_BACKGROUND = (127, 127, 127)
+
+# Map constantes
+TILE_SIZE = 100
+ROWS = 18
+COLS = 15
+
+
+def update(screen, camera_group):
+	"""
+	Actualise chaque élèments du jeu 
+	"""
+
+	screen.fill(COLOR_BACKGROUND)
+	camera_group.update()
+	camera_group.custom_draw()
+
+	pygame.display.update()
 
 
 
+
+def start_game():
+	"""
+	Fonction principal du jeu
+	"""
+
+	# Setup pygame
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	pygame.display.set_caption("yorg.io")
+	clock = pygame.time.Clock()
+	pygame.event.set_grab(True)
+	
+	camera_group = CameraGroup()
+
+	running = True
+	while running:
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				running = False
+
+		if event.type == pygame.MOUSEWHEEL:
+			camera_group.zoom_scale += event.y * 0.03
+
+
+		update(screen, camera_group)
+
+
+		clock.tick(60)
 
 
 
 if __name__ == '__main__':
-	create_map.create_map()
+	start_game()
+	pygame.quit()
